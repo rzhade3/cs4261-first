16f2d8398b89709c63b5e20643e5f7e11ab2af75
Rahul Zhade <rzhade3@users.noreply.github.com>
2019-09-01 14:14:38 -0400

    Changed port number of frontend

--- frontend/index.js
+++ frontend/index.js
@@ -3,6 +3,8 @@ var request = require('superagent');
 
 var app = express();
 
+var port = process.env.PORT || 8080;
+
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/public/views/');
 
@@ -12,4 +14,4 @@ app.get('/', function(req, res){
   res.render('index');
 });
 
-app.listen(3000);
+app.listen(port);
